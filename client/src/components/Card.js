582ecf2953500857aa94141a9cca0510a8086963
Jonathan Morales <jonmorazav@yahoo.com>
2019-03-02 17:26:08 -0500

    trying time ago logic

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,6 +1,23 @@
 import React from "react";
-import "./card.css";
+import "./Card.css";
+
 function Card(props) {
+    const getAgo = milliseconds => {
+        let unit = "";
+        let time = 0;
+        let seconds = (new Date().getTime() - seconds) / 1000;
+        if(seconds < 60 ) {
+            unit = "s";
+            time = seconds
+        } else if (seconds > 60 && seconds < 3600){
+            unit = " mins";
+            time = seconds / 60;
+        } else {
+            unit = " hrs";
+            time = seconds / 3600;
+        }
+        return time + unit;
+    }
   return (
     <div className="container">
       <div className="column">
@@ -23,8 +40,7 @@ function Card(props) {
                 {props.obj.user}
               </span>
               <span className="timestamp">
-                <i className="fa fa-clock-o" /> {props.obj.timeAgo}
-                {props.uniOfTimeAgo} ago
+                <i className="fa fa-clock-o" /> {getAgo} ago
               </span>
             </div>
           </div>
